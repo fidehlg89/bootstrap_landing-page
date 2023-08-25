827ead9b4443ab3a4104b978b9d655e9613aab0d
Fidel Ernesto <fidehlg89@gmail.com>
2023-08-25 19:58:46 -0400

    fix: skills updated

--- assets/js/react/skills.js
+++ assets/js/react/skills.js
@@ -21,26 +21,28 @@ const Skillslist = () => {
   const skills1 = [
     { name: "HTML", level: 85 },
     { name: "CSS", level: 80 },
-    { name: "JS", level: 65 },
-    { name: "Vue js", level: 50 },
+    { name: "JS", level: 70 },
+    { name: "JQuery", level: 45 },
+    { name: "Vue js", level: 60 },
     { name: "React js", level: 60 },
-    { name: "TS", level: 30 },
-    { name: "PHP", level: 65 },
-    { name: "Yii2", level: 45 },
-    { name: "Laravel", level: 50 },
-    { name: "Symfony", level: 30 },
+    { name: "TS", level: 60 },
+    { name: "PHP", level: 70 },
+    { name: "Yii2", level: 40 },
+    { name: "Laravel", level: 60 },
+    { name: "Symfony", level: 30 }
   ];
   const skills2 = [
     { name: "Python", level: 30 },
     { name: "Django", level: 35 },
     { name: "Ruby", level: 20 },
-    { name: "RoR", level: 20 },
+    { name: "Ruby o Rails", level: 20 },
     { name: "Java", level: 30 },
     { name: "C#", level: 55 },
     { name: ".Net Core", level: 45 },
     { name: "Blazor WASM", level: 55 },
     { name: "Xamarin", level: 35 },
     { name: "Wordpress/CMS", level: 75 },
+    { name: "SQL", level: 55 }
   ];
 
   return (
